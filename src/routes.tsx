3d9fb8016e06a04d1210a85f975939f252813d14
jaovitim <joaorodriguesjv590@gmail.com>
2026-03-15 18:34:19 -0300

    just a h1 in modesPage

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,15 @@
 import { createBrowserRouter } from "react-router-dom";
 import { Layout } from "./components/layout/Layout";
 import { Home } from "./pages/Home";
+import { Modes } from "./pages/Modes";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     Component: Layout,
-    children: [{ index: true, Component: Home }],
+    children: [
+      { index: true, Component: Home },
+      { path: "/modos", Component: Modes },
+    ],
   },
 ]);
